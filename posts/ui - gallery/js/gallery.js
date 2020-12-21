437a65da6e39d906aa6425e2e104fe94e666c569
Nguyen Van Huyen <huyennv9@cyberspace.vn>
2020-12-21 17:17:28 +0700

    Update gallery

--- posts/ui - gallery/js/gallery.js
+++ posts/ui - gallery/js/gallery.js
@@ -1,62 +1,73 @@
 (function () {
-  var data = [
-    {
-      title: "Portfolio",
-      image: "images/myblogger.jpg",
-      link: ""
-    },
-    {
-      title: "",
-			image: "images/home2.jpg",
-			link: ""
-		},
+    const data = [
+        {
+            title: 'Portfolio',
+            image: 'images/myblogger.jpg',
+            link: 'https://lockex1987.github.io/'
+        },
+        {
+            title: '',
+            image: 'images/home2.jpg',
+            link: ''
+        },
 
-		{
-			title: "",
-			image: "images/home3.jpg",
-			link: ""
-		},
-		{
-			title: "",
-			image: "images/home4.jpg",
-			link: ""
-		},
-		{
-			title: "",
-			image: "images/home5.jpg", link: ""
-		},
-		{
-			title: "",
-			image: "images/home6.jpg", link: ""
-		},
-		{
-			title: "",
-			image: "images/home7.jpg", link: ""
-		},
-		{ title: "", image: "images/home8.jpg", link: "" }
-	];
-	
-	var gallery = document.getElementById("gallery");
-	for (var i = 0; i < data.length; i++) {
-		var e = data[i];
-		var img = document.createElement("img");
-		img.src = e.image;
-		var figure = document.createElement("figure");
-		var figcaption = document.createElement("figcaption");
-		figure.appendChild(img);
-		figure.appendChild(figcaption);
-		var li = document.createElement("li");
-		if (e.title) {
-			figcaption.textContent = e.title;
-			var a = document.createElement("a");
-			a.href = e.link;
-			a.appendChild(figure);
-			li.appendChild(a);
-		} else {
-			figcaption.textContent = "Comming soon";
-			li.appendChild(figure);
-		}
+        {
+            title: '',
+            image: 'images/home3.jpg',
+            link: ''
+        },
+        {
+            title: '',
+            image: 'images/home4.jpg',
+            link: ''
+        },
+        {
+            title: '',
+            image: 'images/home5.jpg',
+            link: ''
+        },
+        {
+            title: '',
+            image: 'images/home6.jpg',
+            link: ''
+        },
+        {
+            title: '',
+            image: 'images/home7.jpg',
+            link: ''
+        },
+        {
+            title: '',
+            image: 'images/home8.jpg',
+            link: ''
+        }
+    ];
 
-		gallery.appendChild(li);
-	}
+    const gallery = document.getElementById('gallery');
+    for (let i = 0; i < data.length; i++) {
+        const e = data[i];
+        const img = document.createElement('img');
+        img.src = e.image;
+        const figure = document.createElement('figure');
+        figure.className = 'mx-0';
+        const figcaption = document.createElement('figcaption');
+        figcaption.className = 'p-3 mt-0';
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        const li = document.createElement('div');
+        li.className = 'mb-3 col-6 col-lg-4';
+        if (e.title) {
+            figcaption.textContent = e.title;
+            const a = document.createElement('a');
+            a.className = 'text-decoration-none';
+            a.href = e.link;
+            a.appendChild(figure);
+            li.appendChild(a);
+        } else {
+            figcaption.textContent = 'Comming soon';
+            li.appendChild(figure);
+        }
+
+        gallery.appendChild(li);
+    }
 })();
