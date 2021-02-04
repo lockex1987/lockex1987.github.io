bf3573bb9c3a84e3f883e5f8a76a7311adb8723a
Nguyen Van Huyen <huyennv9@cyberspace.vn>
2021-02-04 08:28:50 +0700

    Update file manager

--- posts/project - file manager/server/list_files.php
+++ posts/project - file manager/server/list_files.php
@@ -7,9 +7,19 @@ $folder = $_GET['folder'];
 // Đọc các file trong thư mục và đẩy vào mảng
 $files = scandir($rootFolder . $folder);
 
+// Các thư mục hệ thống, không hiển thị cho người dùng
+$HIDDEN_FOLDERS = [
+    '.',
+    '..',
+    'lost+found',
+    '.Trash-1000',
+    'System Volume Information',
+    '$RECYCLE.BIN'
+];
+
 $result = [];
 foreach ($files as $file) {
-    if (!in_array($file, ['.', '..', 'lost+found', '.Trash-1000'])) {
+    if (!in_array($file, $HIDDEN_FOLDERS)) {
         $absPath = $rootFolder . $folder . $file;
         $isDir = is_dir($absPath);
         array_push($result, [
