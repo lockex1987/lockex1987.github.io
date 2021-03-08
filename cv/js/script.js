4c2712340d841ee3ec91069ac0cee3728c00056a
Nguyen Van Huyen <huyennv9@cyberspace.vn>
2021-03-08 13:56:13 +0700

    Cap nhat CV nhung gi tap trung

--- cv/js/script.js
+++ cv/js/script.js
@@ -1,40 +1,49 @@
-// Danh sách các blog
-const timelineBlocks = document.querySelectorAll('.timeline-container');
+// Xử lý chỗ timeline
+(function () {
+    // Danh sách các block
+    const timelineBlocks = document.querySelectorAll('.timeline-container');
 
-function isBelow(el) {
-    const top = el.getBoundingClientRect().top + window.pageYOffset - document.documentElement.clientTop;
-    const scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-    const height = window.innerHeight;
-    return top > scrollTop + height * 0.75;
-}
 
-function isHidden(el) {
-    return el.classList.contains('invisible');
-}
-
-function hideBlock(el) {
-    el.classList.add('invisible');
-}
-
-function showBlock(el) {
-    el.classList.remove('invisible');
-    el.classList.add('bounce-in');
-}
-
-// Hide timeline blocks which are outside the viewport
-timelineBlocks.forEach((el) => {
-    if (isBelow(el)) {
-        hideBlock(el);
-    } else {
-        showBlock(el);
+    function isBelow(el) {
+        const top = el.getBoundingClientRect().top + window.pageYOffset - document.documentElement.clientTop;
+        const scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+        const height = window.innerHeight;
+        return top > scrollTop + height * 0.75;
     }
-});
 
-// On scolling, show/animate timeline blocks when enter the viewport
-window.addEventListener('scroll', () => {
+
+    function isHidden(el) {
+        return el.classList.contains('invisible');
+    }
+
+
+    function hideBlock(el) {
+        el.classList.add('invisible');
+    }
+
+
+    function showBlock(el) {
+        el.classList.remove('invisible');
+        el.classList.add('bounce-in');
+    }
+
+
+    // Hide timeline blocks which are outside the viewport
     timelineBlocks.forEach((el) => {
-        if (!isBelow(el) && isHidden(el)) {
+        if (isBelow(el)) {
+            hideBlock(el);
+        } else {
             showBlock(el);
         }
     });
-});
+
+
+    // On scolling, show/animate timeline blocks when enter the viewport
+    window.addEventListener('scroll', () => {
+        timelineBlocks.forEach((el) => {
+            if (!isBelow(el) && isHidden(el)) {
+                showBlock(el);
+            }
+        });
+    });
+})();
--- js/posts.js
+++ js/posts.js
@@ -102,7 +102,7 @@ const App = {
             postList: null,
 
             // Tìm kiếm local (file JSON), không phải Elasticsearch
-            localSearch: false
+            localSearch: true
         };
     },
 
