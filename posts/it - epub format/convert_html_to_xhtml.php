744df5665229561738d5f2531ef008551fd47865
Nguyen Van Huyen <huyennv9@cyberspace.vn>
2021-03-16 14:16:21 +0700

    Delete highlight.js library files

---
+++ posts/it - epub format/convert_html_to_xhtml.php
@@ -0,0 +1,38 @@
+<?php
+
+// https://en.wikipedia.org/wiki/XHTML
+// http://www.w3.org/People/Raggett/tidy/
+// http://tidy.sourceforge.net/libintro.html
+// https://www.php.net/tidy
+function convertHtmlToXhtml(string $htmlCode): string
+{
+    $tidy = new Tidy();
+    // http://tidy.sourceforge.net/docs/quickref.html
+    $options = [
+        'indent' => true,
+        'output-xhtml' => true,
+        // 'output-xml' => true
+        'show-body-only' => true
+    ];
+    $tidy->parseString($htmlCode, $options, 'utf8');
+    $tidy->cleanRepair();
+
+    /*
+    if ($tidy->errorBuffer) {
+        echo 'There are some errors!' . PHP_EOL;
+        $errors = explode(PHP_EOL, $tidy->errorBuffer);
+
+        foreach ($errors as $error) {
+            echo $error . PHP_EOL;
+        }
+    } else {
+        echo 'There are no errors.';
+    }
+    */
+
+    return tidy_get_output($tidy);
+}
+
+
+$htmlCode = 'syntax <strong>error</small> <myowntag>my text</myowntag>';
+echo convertHtmlToXhtml($htmlCode) . PHP_EOL;
