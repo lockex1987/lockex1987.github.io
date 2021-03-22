2bd006b16ffb94b98bed9b79cc859ccd158505af
Nguyen Van Huyen <huyennv9@cyberspace.vn>
2021-03-22 12:37:48 +0700

    Co the export Excel thu HTML table voi PhpSpreadsheet, chua ve duoc bieu do

--- posts/php - excel/phpspreadsheet/phpspreadsheet_demo.php
+++ posts/php - excel/phpspreadsheet/phpspreadsheet_demo.php
@@ -1,33 +1,101 @@
 <?php
+
 require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
+
 function simpleDemo()
 {
     $spreadsheet = new Spreadsheet();
-
     $sheet = $spreadsheet->getActiveSheet();
     $sheet->setCellValue('A1', 'Hello World !');
-    
     $writer = new Xlsx($spreadsheet);
     $writer->save('demo_1.xlsx');
 }
 
+
 function readExistingFile()
 {
-    $filePath = 'demo_1.xlsx';
-    $spreadsheet = IOFactory::load($filePath);
-
+    $inputFilePath = 'demo_1.xlsx';
+    $spreadsheet = IOFactory::load($inputFilePath);
     $sheet = $spreadsheet->getActiveSheet();
     $sheet->setCellValue('B2', 'Updated');
 
+    $outputFilePath = 'demo_2.xlsx';
     $writer = new Xlsx($spreadsheet);
-    $writer->save('demo_2.xlsx');
+    $writer->save($outputFilePath);
 }
 
-simpleDemo();
-readExistingFile();
 
+/**
+ * Dùng Html Reader của PhpSpreadsheet.
+ */
+function generateFromHtml()
+{
+    // Color phải dạng 6 chữ số hexa
+    $htmlString = <<<'HTML'
+        <table>
+            <tr>
+                <td style="background-color: #4c87b9; color: #fff; width: 45; text-align: center"
+                    colspan="2">
+                    Hello World
+                </td>
+            </tr>
+            <tr>
+                <td style="color: red"
+                    rowspan="2">
+                    Hello
+                    <br />
+                    World
+                </td>
+            </tr>
+            <tr>
+                <!-- Vẫn phải có cell trống? -->
+                <!--td>
+                    
+                </td-->
+                <td style="border: 1px solid #000000;">
+                    Hello
+                    <br>
+                    World
+                </td>
+            </tr>
+        </table>
+        HTML;
+
+    $reader = new \PhpOffice\PhpSpreadsheet\Reader\Html();
+    
+    $spreadsheet = $reader->loadFromString($htmlString);
+
+    // $reader->setSheetIndex(1);
+    // $spreadhseet = $reader->loadFromString($secondHtmlString, $spreadsheet);
+
+    $writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xlsx');
+    $writer->save('write.xlsx');
+}
+
+
+function drawChart()
+{
+    $inputFilePath = 'chart_input.xlsx';
+    $spreadsheet = IOFactory::load($inputFilePath);
+
+    /*
+    $sheet = $spreadsheet->setActiveSheetIndex(1);
+    $sheet->setCellValue('A3', 'Bình');
+    $sheet->setCellValue('B3', 200);
+    */
+
+    $outputFilePath = 'chart_output.xlsx';
+    $writer = new Xlsx($spreadsheet);
+    $writer->save($outputFilePath);
+}
+
+
+// simpleDemo();
+// readExistingFile();
+generateFromHtml();
+drawChart();
