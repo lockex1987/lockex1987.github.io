b0486dd949a1c4024efd8e51c45f667b20d6caa7
Nguyen Van Huyen <huyennv9@cyberspace.vn>
2021-04-23 13:48:03 +0700

    Fix bug 100vh

--- posts/project - playlist/js/script.js
+++ posts/project - playlist/js/script.js
@@ -9,3 +9,16 @@ function init() {
 
 // window.onYouTubeIframeAPIReady = init;
 init();
+
+
+/**
+ * Sửa lỗi 100vh trên thiết bị iOS Safari:
+ *   https://viblo.asia/p/sua-loi-100vh-tren-thiet-bi-ios-safari-gGJ59GEjZX2.
+ */
+function calculateWindowHeight() {
+    const doc = document.documentElement;
+    doc.style.setProperty('--window-height', `${window.innerHeight}px`);
+}
+
+window.addEventListener('resize', calculateWindowHeight);
+calculateWindowHeight();
