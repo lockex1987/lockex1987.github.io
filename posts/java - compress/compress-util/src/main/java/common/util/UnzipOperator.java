81b2952209b6ae0ae88fbe9b9dcd676de89b377f
Nguyen Van Huyen <huyennv9@cyberspace.vn>
2021-05-19 17:02:52 +0700

    Bo Java

--- posts/java - compress/compress-util/src/main/java/common/util/UnzipOperator.java
+++
@@ -1,9 +0,0 @@
-package common.util;
-
-import java.io.InputStream;
-import java.util.zip.ZipEntry;
-
-public interface UnzipOperator {
-
-	void read(ZipEntry ze, InputStream is);
-}
--- posts/java - maven/maven - build jar package/source/build-jar/src/main/java/cttd/maven/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package cttd.maven;
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("Hello world");
-	}
-}
