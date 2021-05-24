72480a86dd5e915684b025e6b441b16fb37f304a
Nguyen Van Huyen <huyennv9@cyberspace.vn>
2021-05-24 15:11:54 +0700

    Parallax, sticky

--- posts/ui - multi layered parallax/js/script.js
+++ posts/ui - multi layered parallax/js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function (evt) {
-    var topDistance = this.pageYOffset;
+    const topDistance = this.pageYOffset;
     document.querySelectorAll('.layer').forEach(layer => {
-        var depth = layer.dataset.depth;
+        const depth = layer.dataset.depth;
         layer.style.transform = 'translate3d(0, ' + -(topDistance * depth) + 'px, 0)';
     });
 });
