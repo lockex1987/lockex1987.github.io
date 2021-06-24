0a1ded79535895dae35cd9727a57c4658e771ebb
Nguyen Van Huyen <huyennv9@cyberspace.vn>
2021-06-24 15:44:51 +0700

    Bo image map, echarts

--- tr.sh
+++ tr.sh
@@ -1,8 +1,8 @@
-# php rename rc program/comani/
-# php rename x program/comani/
 PHP_PATH=/d/rename
 ROOT_FOLDER=/d/new/comani
 
+# php rename x program/comani/
+php $PHP_PATH rc $ROOT_FOLDER
 php $PHP_PATH lc $ROOT_FOLDER
 for f in $ROOT_FOLDER/*; do php $PHP_PATH pr "$f" "z"; done
 for f in $ROOT_FOLDER/*; do php $PHP_PATH sf "$f"; done
