17421fa56d0a6c6f93c6a3ff7a4b94a59bdbe144
Nguyen Van Huyen <huyennv9@cyberspace.vn>
2021-07-16 17:30:18 +0700

    Masonry

--- posts/ui - masonry/js/script.js
+++ posts/ui - masonry/js/script.js
@@ -1,35 +1,60 @@
-function getImageUrls() {
-    let urls = [];
-    let imageNum = 27;
-    for (let i = 0; i < imageNum; i++) {
-        let j = i;
-        //var j = Math.floor(Math.random() * IMAGES.length);
-        let u = IMAGES[j];
-        urls.push(u);
+import IMAGES from './data.js';
+
+new Vue({
+    el: '#app',
+
+    data() {
+        return {
+            urls: [],
+
+            // Đối tượng Masonry
+            masonry: null
+        };
+    },
+
+    mounted() {
+        this.initMasonry();
+        this.urls = this.getImageUrls();
+
+        this.$nextTick(() => {
+            this.reLayout();
+        });
+    },
+
+    methods: {
+        getImageUrls() {
+            const urls = [];
+            const imageNum = 27;
+            for (let i = 0; i < imageNum; i++) {
+                const j = i;
+                // const j = Math.floor(Math.random() * IMAGES.length);
+                urls.push(IMAGES[j]);
+            }
+            return urls;
+        },
+
+        /**
+         * Khởi tạo đối tượng Masonry.
+         */
+        initMasonry() {
+            const elem = document.querySelector('.masonry-js');
+            this.masonry = new Masonry(elem, {
+                itemSelector: '.masonry-item'
+            });
+        },
+
+        /**
+         * Hiển thị lại.
+         */
+        reLayout() {
+            this.masonry.reloadItems();
+            this.masonry.layout();
+        },
+
+        checkLoadImagesComplete() {
+            setTimeout(() => {
+                this.reLayout();
+            }, 100);
+        }
     }
-    return urls;
-}
-
-function bindHorizontalMasonry(urls) {
-    let html = urls.reduce((s, url) => {
-        return s + `<div class="image-thumb" style="background-image:url(${url});">
-                        <img src="${url}">
-                    </div>`;
-    }, '');
-    document.querySelector('.masonry-horizontal').innerHTML = html;
-}
-
-function bindVerticalMasonry(urls) {
-    let html = urls.reduce((s, url) => {
-        return s + `<img src="${url}">`;
-    }, '');
-    document.querySelector('.masonry-vertical').innerHTML = html;
-}
-
-function initMasonry() {
-    let urls = getImageUrls();
-    bindVerticalMasonry(urls);
-    bindHorizontalMasonry(urls);
-}
-
-initMasonry();
+});
